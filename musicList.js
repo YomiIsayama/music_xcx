be57a2da63d9325e19c5edebcc75ea6399c66b8b
YomiIsayama <scepters1@gmail.com>
2020-04-22 11:18:29 +0800

    music_xcx

---
+++ musicList.js
@@ -0,0 +1,29 @@
+// 播放列表数据
+let musicList= [{
+  id: 1,
+  title: '11111',
+  singer: '1111',
+  src: 'http://xxxxxx.mp3',
+  coverImgUrl: 'http://xxxxxx/1.jpeg'
+}, {
+  id: 2,
+  title: '22222222',
+  singer: '22222222',
+  src: 'http://xxxxxxx/2.mp3',
+    coverImgUrl: 'http://xxxxxxxx/2.jpeg'
+}, {
+  id: 3,
+  title: '33333333',
+  singer: '3333333333',
+  src: 'http://xxxxxx/3.mp3',
+    coverImgUrl: 'http://xxxxxxx/3.jpeg'
+}, {
+  id: 4,
+  title: '444444444444',
+  singer: '44444444',
+  src: 'http://xxxxxxxxxxx/4.mp3',
+    coverImgUrl: 'http://xxxxxxx/4.jpeg'
+}]
+module.exports={
+  musicList
+}
